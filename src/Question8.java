1c21e734b56b607efb88a45a96c8998d627d2a1c
edu30311 <202430311@email.daelim.ac.kr>
2024-10-04 10:04:17 +0900

    Initial commit

--- src/Question8.java
+++ src/Question8.java
@@ -2,13 +2,12 @@ public class Question8 {
     public static void main(String[] args) {
         int i = 0;
         int num = 0;
-        while (i <= 100){
+        while (i < 100){
             i++;
-            if (i % 2 == 0){
+            if (i % 2 != 0){
+                num += i;
                 continue;
             }
-            num += i;
-
         }
         System.out.println(num);
     }
