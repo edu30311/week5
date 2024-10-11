99971b1ea316a9bf2ed5cfebdcd00f1e9910be1b
edu30311 <202430311@email.daelim.ac.kr>
2024-10-11 14:42:18 +0900

    Initial commit

--- src/Question10.java
+++ src/Question10.java
@@ -8,25 +8,12 @@ public class Question10 {
         int number = sc.nextInt();
         int sum = 0;
 
-        /*for (int i = 1; i <= number; i++) {
-            if (sum > 100) {
-                System.out.println(sum);
-                break;
-            }
-            sum += i;
-        }*/
         for (int i = 1; i <= number; i++) {
+            if (sum > 100) {
+                break;
+            }
             sum += i;
-            if (i < number) {
-                continue;
-            }
-            else if (sum > 100) {
-                System.out.println(sum);
-                break;
-            }
-            else {
-                break;
-            }
         }
+        System.out.println(sum);
     }
 }
